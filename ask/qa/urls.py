bf89ba5e8d7e1d5256092b9b003409b904360261
FF7FSystem <yellowbear@yandex.ru>
2016-11-22 22:17:15 +0700

    end 22.11.

---
+++ ask/qa/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from qa.views import test
+
+urlpatterns = [
+    url(r'^$',test, name='test'),
+    url(r'^question/(?P<pk>\d+)/',  test, name='test'),
+    url(r'^popular/', test, name='test'),
+    url(r'^ask/',  test, name='test'),
+    url(r'^signup/',  test, name='test'),
+    url(r'^login/',  test, name='test'),
+    url(r'^new/', test, name='new'),
+]
--- init.sh
+++ init.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
+touch /home/box/nginx.log
+# Nginx
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
+
+#Gunicorn 
+touch /home/box/gunicorn.log
+
+sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
